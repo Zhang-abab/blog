747542bece4b0aea380d9989f6b640abae3f9375
Zhang_abab <2411358676@qq.com>
2022-04-16 18:15:44 +0800

    注册

--- app01/views.py
+++ app01/views.py
@@ -1,11 +1,10 @@
-from calendar import c
-from urllib import request
 from django.shortcuts import render, HttpResponse
-from django.core.exceptions import NON_FIELD_ERRORS, ValidationError
 from django.http import JsonResponse
 from app01.utils.random_code import random_code
-from app01.utils.mqtt import led
 from django import forms
+from django.contrib import auth
+from app01.utils.mqtt import led
+from app01.models import UserInfo
 # Create your views here.
 
 def index(request):
@@ -15,8 +14,8 @@ def index(request):
 def news(request):
 
     return render(request,'news.html')
-
-class loginForm(forms.Form):
+#登陆的字段验证
+class LoginForm(forms.Form):
     name = forms.CharField(error_messages={'required':'请输入用户名'})
     pwd = forms.CharField(error_messages={'required':'请输入密码'})
     code = forms.CharField(error_messages={'required':'请输入验证码'})
@@ -28,7 +27,6 @@ class loginForm(forms.Form):
 
         super().__init__(*args,**kwargs)
         #写自己的方法
-
 
     #配置局部钩子
     def clean_code(self):
@@ -42,11 +40,16 @@ class loginForm(forms.Form):
     def clean(self):
         name = self.cleaned_data.get('name')
         pwd = self.cleaned_data.get('pwd')
-        if name != '1234' or pwd != '1234':
+
+        user = auth.authenticate(username = name, password = pwd)
+        if not user:
             #为字段添加错误信息
             self.add_error('pwd', '用户名或密码错误')
-        return self.cleaned_data
+            return self.cleaned_data
 
+        #把用户对象放到cleaned_data
+        self.cleaned_data['user'] = user
+        return self.cleaned_data
 #登陆失败
 def clean_form(form):
     err_dict:dict = form.errors
@@ -62,15 +65,75 @@ def login(request):
             "msg":"登陆成功",
             'self':None,
         }
-        form = loginForm(request.data, request = request)
+        form = LoginForm(request.data, request = request)
         if not form.is_valid():
             res['self'],res['msg'] = clean_form(form)
             return JsonResponse(res)
+
+        #写登陆操作
+        user = (form.cleaned_data.get('user'))
+        auth.login(request, user)
         res['code'] = 0
         return JsonResponse(res)
     return render(request, 'login.html')
 
+#注册的字段验证
+class SignForm(forms.Form):
+    name = forms.CharField(error_messages={'required': '请输入用户名'})
+    pwd = forms.CharField(error_messages={'required': '请输入密码'})
+    re_pwd = forms.CharField(error_messages={'required': '请再次输入密码'})
+    code = forms.CharField(error_messages={'required': '请输入验证码'})
+
+    def __init__(self, *args, **kwargs):
+
+        self.request = kwargs.pop('request',None)
+
+        super().__init__(*args,**kwargs)
+        #写自己的方法
+
+    def clean(self):
+        pwd = self.cleaned_data.get('pwd')
+        re_pwd = self.cleaned_data.get('re_pwd')
+        if pwd != re_pwd:
+            self.add_error('re_pwd', '两次输入的密码不一致')
+        return self.cleaned_data
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        user_query = UserInfo.objects.filter(username = name)
+        if user_query:
+            self.add_error('name','该用户已注册')
+
+        return self.cleaned_data
+
+    def clean_code(self):
+        code:str = self.cleaned_data.get('code')
+        valid_code:str = self.request.session.get('valid_code')
+        if code.upper() != valid_code.upper():
+            self.add_error('code','验证码输入错误')
+        return self.cleaned_data
+
 def sign(request):
+    if request.method == 'POST':
+        res = {
+            'code':425,
+            'msg':"登陆成功",
+            'self':None
+        }
+        form = SignForm(request.data, request = request)
+        if not form.is_valid():
+            res['self'], res['msg'] = clean_form(form)
+            return JsonResponse(res)
+
+        #注册成功的代码
+        user = UserInfo.objects.create_user(
+            username = request.data.get('name'),
+            password = request.data.get('pwd')
+        )
+        #注册之后自动登录
+        auth.login(request, user)
+        res['code'] = 0
+        return JsonResponse(res)
     return render(request, 'sign.html')
 
 def get_random_code(request):
@@ -80,7 +143,6 @@ def get_random_code(request):
 
 def mqtt(request):
     return render(request,'Led.html')
-
 
 def mqtt_led(request,pin):
     if pin == 1:
