834ec850a2c4243211c44860967fd105dc4e2eae
Zhang_abab <2411358676@qq.com>
2022-05-09 18:54:32 +0800

    test

--- api/views/api_email.py
+++ api/views/api_email.py
@@ -5,12 +5,14 @@ from django import forms
 
 
 class EmailForm(forms.Form):
-    email = forms.EmailField(error_messages={'required': '请输入邮箱'})
+    email = forms.EmailField(error_messages={'required': '请输入邮箱', 'invalid': '请输入正确邮箱'})
 
 
 class ApiEmail(View):
     def post(self, request):
         email = request.data.get('email')
-        EmailForm(email)
-        print(email)
+        form = EmailForm(request.data)
+        if form.is_valid():
+            clean_form(form)
+
         return JsonResponse({})
